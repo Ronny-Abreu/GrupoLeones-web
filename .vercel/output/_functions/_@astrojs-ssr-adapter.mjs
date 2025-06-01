c1b012429ec8d3f59d7f6d502d90734b60823553
tuerre <ronnyduran35@gmail.com>
2025-06-01 03:38:05 -0400

    update vercel

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_DPe1hLt9.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DRy8AiEC.mjs';
-export { renderers } from '../renderers.mjs';
