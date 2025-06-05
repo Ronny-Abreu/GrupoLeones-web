9eb30a19a322ba2da5a367298bcdd7dd1981de21
tuerre <ronnyduran35@gmail.com>
2025-06-05 14:55:53 -0400

    add SEO sitemap.xml config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: 'https://grupoleones.com.do',
+  integrations: [sitemap()],
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
