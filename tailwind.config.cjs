a0b3e6fb2fb6c043a9832ac33313661f33103165
tuerre <ronnyduran35@gmail.com>
2025-06-05 14:56:10 -0400

    edit tailwind references

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+                        'purple-light': '#e8e5f3',
+                        'purple-medium': '#8b7fb8',
+                        'purple-dark': '#6b5b95'
+                    }
+    },
   },
   plugins: [],
 }
